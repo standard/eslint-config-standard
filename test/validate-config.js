2770d499cc3d3e47bc7c5d30a272e860c18cff73
Luke Karrys <lukekarrys@gmail.com>
2015-08-18 17:16:46 -0700

    Make test check for errorCount of 0

--- test/validate-config.js
+++ test/validate-config.js
@@ -8,6 +8,8 @@ test('load config in eslint to validate all rule syntax is correct', function (t
     configFile: 'eslintrc.json'
   })
 
-  t.ok(cli.executeOnText('var foo\n'))
+  var code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+
+  t.ok(cli.executeOnText(code).errorCount === 0)
   t.end()
 })
