6eaf40b498db582fca66aabb45ddd3b737f3bc27
Feross Aboukhadijeh <feross@feross.org>
2016-02-03 22:19:26 -0800

    code style

--- test/validate-config.js
+++ test/validate-config.js
@@ -1,7 +1,8 @@
+var eslint = require('eslint')
 var test = require('tape')
 
 test('load config in eslint to validate all rule syntax is correct', function (t) {
-  var CLIEngine = require('eslint').CLIEngine
+  var CLIEngine = eslint.CLIEngine
 
   var cli = new CLIEngine({
     useEslintrc: false,
