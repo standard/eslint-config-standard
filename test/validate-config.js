204dee174233ac809b2f1a4ceb9e664bef3a96ae
Feross Aboukhadijeh <feross@feross.org>
2020-10-28 19:31:28 -1000

    fix tests

--- test/validate-config.js
+++ test/validate-config.js
@@ -9,7 +9,7 @@ test('load config in eslint to validate all rule syntax is correct', function (t
     configFile: 'eslintrc.json'
   })
 
-  const code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+  const code = 'const foo = 1\nconst bar = function () {}\nbar(foo)\n'
 
   t.equal(cli.executeOnText(code).errorCount, 0)
   t.end()
