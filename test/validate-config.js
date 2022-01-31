73eb1e6f3d71b4bbf11f862583f490a19f38b30e
Pelle Wessman <pelle@kodfabrik.se>
2022-01-31 22:30:01 +0100

    Add tests for ensuring top level awaits
    Fixes https://github.com/standard/standard/issues/1548

--- test/validate-config.js
+++ test/validate-config.js
@@ -8,3 +8,11 @@ test('load config in eslint to validate all rule syntax is correct', async funct
   t.equal(lintResult.errorCount, 0)
   t.end()
 })
+
+test('ensure we allow top level await', async function (t) {
+  const eslint = new ESLint()
+  const code = 'const foo = await 1\nconst bar = function () {}\nawait bar(foo)\n'
+  const [lintResult] = await eslint.lintText(code)
+  t.equal(lintResult.errorCount, 0)
+  t.end()
+})
